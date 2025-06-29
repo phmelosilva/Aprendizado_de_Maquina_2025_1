0d436afbcb12eb295c9b5feee11d48fd96f460ed
Henrique Torres <henriquetlandin@gmail.com>
2025-06-29 14:51:40 -0300

    feat: add react-toastify for toast notifications in the application

--- mini_trabalho8/disposicao-diaria/src/App.tsx
+++ mini_trabalho8/disposicao-diaria/src/App.tsx
@@ -1,9 +1,11 @@
 import { Form } from "@/components/form";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
     <div className="bg-[#EFEFEF] w-screen h-screen flex items-center justify-center">
       <Form />
+      <ToastContainer />
     </div>
   );
 }
